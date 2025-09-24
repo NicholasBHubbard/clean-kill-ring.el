21bfd7d5041ab152c5b76b6f8f5151f704d8d98d
Nicholas Hubbard <nicholashubbard@posteo.net>
2025-09-24 15:18:18 -0400

    fix bug with kill-whole-line

--- clean-kill-ring.el
+++ clean-kill-ring.el
@@ -9,7 +9,7 @@
 ;; Author: Nicholas Hubbard <nicholashubbard@posteo.net>
 ;; URL: http://github.com/NicholasBHubbard/clean-kill-ring.el
 ;; Package-Requires: ((emacs "24.4"))
-;; Version: 2.4
+;; Version: 2.5
 ;; Created: 2022-02-23
 ;; By: Nicholas Hubbard <nicholashubbard@posteo.net>
 ;; Keywords: kill-ring, convenience
@@ -43,7 +43,8 @@
 Prevents input that matches any of the `clean-kill-ring-filters' from entering
 the `kill-ring'."
   (let ((input (substring-no-properties (car args))))
-    (unless (clean-kill-ring--filter-catch-p input)
+    (unless (or (string-empty-p input)
+                (clean-kill-ring--filter-catch-p input))
       (apply orig-fn args))))
 
 (defvar clean-kill-ring-mode-map (make-sparse-keymap)
